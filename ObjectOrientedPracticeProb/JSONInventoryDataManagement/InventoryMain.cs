fc3c475b8f791949ded4c9aad223886c8af06377
nibedita260 <deepi.nibedita260.com>
2021-09-27 20:32:33 +0530

    [Nibedita]:Added JSONDeserialization JSONInventoryDataManagement Features Invented

---
+++ ObjectOrientedPracticeProb/JSONInventoryDataManagement/InventoryMain.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ObjectOrientedPracticeProb.JSONInventoryDataManagement
+{
+    class InventoryMain
+    {
+        InventoryModel inventoryModel = new InventoryModel();
+        List<InventoryModel> inventoryModels = new List<InventoryModel>();
+        public void DisplayData(string filepath)
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(filepath))//@"D:\git\Object-OrientedPracticeProb\ObjectOrientedPracticeProb\JSONInventoryDataManagement\Inventory.json"
+                {
+                    var json = r.ReadToEnd();
+                    var items = JsonConvert.DeserializeObject<List<InventoryModel>>(json);
+                    Console.WriteLine("Name\tWeight\tPrice");
+                    foreach(var item in items)
+                    {
+                        Console.WriteLine("{0}" + "\t" + "{1}" + "\t" + "{2}", item.Name, item.Weight, item.Price);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPracticeProb/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ObjectOrientedPracticeProb.JSONInventoryDataManagement
+{
+
+    namespace ObjectOrientedPracticeProb
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                bool isExit = false;
+                int options;
+                while (!isExit)
+                {
+                    Console.WriteLine("Choose 1.JSONInventoryDataManagement");
+                    options = Convert.ToInt32(Console.ReadLine());
+                    switch (options)
+                    {
+                        case 1:
+                            InventoryMain inventoryMain = new InventoryMain();
+                            inventoryMain.DisplayData(@"D:\git\Object-OrientedPracticeProb\ObjectOrientedPracticeProb\JSONInventoryDataManagement\Inventory.json");
+                            break;
+                        default:
+                            Console.WriteLine("Choose valid option");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
